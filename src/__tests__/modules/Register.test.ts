4368cf09e560b06559ac58c06937a4e26bade56f
selirah <selirah@pces.mk>
2023-07-20 11:56:52 +0000

    update

--- src/__tests__/modules/Register.test.ts
+++ src/__tests__/modules/Register.test.ts
@@ -1,6 +1,8 @@
 import { DataSource } from "typeorm";
 import { gqlCall } from "../../test-utils/gqlCall";
 import { initializeDataSource } from "../../test-utils/init";
+import { faker } from "@faker-js/faker";
+import { User } from "../../entity/User";
 
 let conn: DataSource;
 
@@ -25,18 +27,33 @@ mutation($payload: RegisterInput!){
 
 describe("Register", () => {
   it("create user", async () => {
-    console.log(
-      await gqlCall({
-        source: registerMutation,
-        variableValues: {
-          payload: {
-            firstName: "Edward",
-            lastName: "Selirah",
-            email: "edward@example.com",
-            password: "password"
-          }
+    const payload = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+
+    const res = await gqlCall({
+      source: registerMutation,
+      variableValues: {
+        payload
+      }
+    });
+
+    expect(res).toMatchObject({
+      data: {
+        register: {
+          firstName: payload.firstName,
+          lastName: payload.lastName,
+          email: payload.email
         }
-      })
-    );
+      }
+    });
+
+    const dbUser = await User.findOne({ where: { email: payload.email } });
+    expect(dbUser).toBeDefined();
+    expect(dbUser!.confirmed).toBeFalsy();
+    expect(dbUser!.firstName).toBe(payload.firstName);
   });
 });
